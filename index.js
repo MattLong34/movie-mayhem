c72e6d788bef207ebf4daafbdecf6b3c02bce30f
MattLong34 <mdlong34@gmail.com>
2020-12-14 13:49:45 -0700

    fetch from api

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('Hi there!');
+require('dotenv').config()
+
+const api_key = require(API_KEY)
+
+console.log(process.env.API_KEY)
+
+const fetchData = async () => {
+    const response = await axios.get('http://www.omdbapi.com/', {
+        params: {
+            apikey: aeaadcd5,
+            i: 'tt0848228'
+        }
+    })
+    console.log('response.data', response.data)
+}
+
+fetchData()
